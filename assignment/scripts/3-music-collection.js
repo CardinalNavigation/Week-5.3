07faaa64ab02ba399ba995a9294577a90b7041d1
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-08-07 22:03:00 -0500

    added the function to add to the collection, and an object to try it on

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,6 +4,14 @@ let collection = []
 console.log(collection);
 
 //function to add albums to collection
-function addToCollections() {
+function addToCollections(title, artist, yearPublished) {
+    console.log(addToCollections())
+    return "Album Added"
+}
 
+//creating new album with title, artist, and yearPublished
+const album0 = {
+    title: "Dare to Be Stupid",
+    artist: "Weird Al Yankovick",
+    yearPublished: "1985"
 }
