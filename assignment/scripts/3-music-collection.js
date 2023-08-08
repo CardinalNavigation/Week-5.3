44e1b673452f0059ceb6b26989851a0127f9c2f6
Matthew Johnson <mtthw.jhnson@gmail.com>
2023-08-07 22:20:28 -0500

    i think i am doing it right now

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,14 +4,19 @@ let collection = []
 console.log(collection);
 
 //function to add albums to collection
+//this needs to create a new object with the parameters
+//it also needs to push it into the current collection array
 function addToCollections(title, artist, yearPublished) {
-    console.log(addToCollections())
-    return "Album Added"
+    let newAddtion = {
+        Title: title,
+        Artist: artist,
+        Released: yearPublished
+    }
+    collection.push(newAddtion);
+    return newAddtion;
 }
 
+console.log(addToCollections("Dare to Be Stupid", "Weird Al Yankovick", "1985"))
+console.log(collection)
+
 //creating new album with title, artist, and yearPublished
-const album0 = {
-    title: "Dare to Be Stupid",
-    artist: "Weird Al Yankovick",
-    yearPublished: "1985"
-}
